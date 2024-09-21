afc801ada556efca855d1589f84608d16d46d6ec
Rianul Amin <rianulamin.r@gmail.com>
2024-09-21 23:40:45 +0600

    profile card component added

---
+++ exxplora/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CardModule } from 'primeng/card';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LeftMenuComponent } from './components/left-menu/left-menu.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LeftMenuComponent,
+    ProfileCardComponent,
+    HeaderComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
