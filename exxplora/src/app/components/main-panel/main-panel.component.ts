cf2b487e0fdd0d3ec3f38ab8dc67a34cbff40abe
Rianul Amin <rianulamin.r@gmail.com>
2024-09-23 19:10:27 +0600

    minor updates

--- exxplora/src/app/components/main-panel/main-panel.component.ts
+++ exxplora/src/app/components/main-panel/main-panel.component.ts
@@ -28,7 +28,7 @@ export class MainPanelComponent implements AfterViewInit {
     this.cardContainer.nativeElement.addEventListener('wheel', (event: WheelEvent) => {
         if (event.deltaY !== 0) {
             event.preventDefault();
-            const scrollAmount = event.deltaY > 0 ? 50 : -50; 
+            const scrollAmount = event.deltaY > 0 ? 200 : -200; 
             this.cardContainer.nativeElement.scrollLeft += scrollAmount; 
         }
     });
