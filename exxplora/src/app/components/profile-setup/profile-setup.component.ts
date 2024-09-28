ae1030e95aee09d6b59eb084f4e7fd4843f981db
Rianul Amin <rianulamin.r@gmail.com>
2024-09-29 04:06:32 +0600

    profile setup component

--- exxplora/src/app/components/profile-setup/profile-setup.component.ts
+++ exxplora/src/app/components/profile-setup/profile-setup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-setup',
@@ -6,31 +7,89 @@ import { Component } from '@angular/core';
   styleUrl: './profile-setup.component.css'
 })
 export class ProfileSetupComponent {
+
+  profileForm!: FormGroup;
   active: number = 0;
+  location: string = '';
+  organization: string = '';
+  institution: string = '';
+  startYear: string = '';
+  endYear: string = '';
 
-    name: string = "";
+  option1: boolean | undefined = false;
+  option2: boolean | undefined = false;
+  option3: boolean | undefined = false;
+  option4: boolean | undefined = false;
+  option5: boolean | undefined = false;
+  option6: boolean | undefined = false;
+  option7: boolean | undefined = false;
+  option8: boolean | undefined = false;
+  option9: boolean | undefined = false;
+  option10: boolean | undefined = false;
 
-    email: string = "";
+  isStudent: boolean = false;
 
-    password: string = "";
+  constructor(private fb: FormBuilder) {}
 
-    option1: boolean | undefined = false;
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      location: ['', Validators.required],  
+      organization: ['', this.organizationValidator.bind(this)],  
+      institution: ['', this.institutionValidator.bind(this)], 
+      startYear: [''],
+      endYear: [''],
+    });
+  }
 
-    option2: boolean | undefined = false;
+  organizationValidator(control: any) {
+    if (!this.isStudent && !control.value) {
+      return { required: true };
+    }
+    return null;
+  }
 
-    option3: boolean | undefined = false;
+  institutionValidator(control: any) {
+    if (this.isStudent && !control.value) {
+      return { required: true };
+    }
+    return null;
+  }
 
-    option4: boolean | undefined = false;
+  setStudentStatus(status: boolean): void {
+    this.isStudent = status;
+    this.profileForm.get('organization')?.updateValueAndValidity();
+    this.profileForm.get('institution')?.updateValueAndValidity();
+    this.profileForm.get('startYear')?.updateValueAndValidity();
+  }
 
-    option5: boolean | undefined = false;
+  onProfilePicSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      console.log("Profile picture selected:", file);
+    }
+  }
 
-    option6: boolean | undefined = false;
+  onCoverPicSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      console.log("Cover photo selected:", file);
+    }
+  }
 
-    option7: boolean | undefined = false;
+  onSubmitStep1(step1Form: any) {
+    if (step1Form.valid) {
+      console.log('Form is valid, moving to the next step');
+      this.active += 1;
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
-    option8: boolean | undefined = false;
-
-    option9: boolean | undefined = false;
-
-    option10: boolean | undefined = false;
+  onNext(): void {
+    if (this.profileForm.valid) {
+      console.log('Form is valid, proceed to next step.');
+    } else {
+      console.log('Form is invalid, please correct the errors.');
+    }
+  }
 }
