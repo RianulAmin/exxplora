a42623c9de2ce221f040699a3993e010696f82df
This is Ratul <newidratul@gmail.com>
2024-09-29 18:14:51 +0600

    profilePopulated

--- exxplora/src/app/components/profile-card/profile-card.component.ts
+++ exxplora/src/app/components/profile-card/profile-card.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { UserService } from './../../services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { GetInfo } from '../../interfaces/get-info';
 
 @Component({
   selector: 'app-profile-card',
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.css'
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnInit {
+  constructor (private userService:  UserService) {}
 
+  userDetails: GetInfo = {
+    Id: 0, 
+    FirstName: '',
+    LastName: '',
+    OrganizationName: '',
+    Location: '',
+    ProfilePicUrl: '',
+    CoverPicUrl: ''
+  };
+
+
+  ngOnInit(): void {
+    this.userService.getUserData().subscribe(
+      res => {
+        console.log(res);
+        if(!res.IsError){
+          this.userDetails = res.Data;
+        }
+        else alert("Failed to load")
+      },
+      err => {
+        alert("Failed to load")
+      }
+    )
+  } 
 }
